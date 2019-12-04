88ddf3612630cdae8cd9e7e063297345a385f78b
Christos Ploutarchou <cploutarchou@gmail.com>
2019-12-05 00:19:20 +0200

    Updated event listener and add next steps on function ctrlAddItem function. Also added new event listener for enter key

--- app.js
+++ app.js
@@ -15,13 +15,24 @@ const UIController = (function () {
 // MAIN APPLICATION CONTROLLER
 const controller = (function (budgetCtrl, UICtrl) {
 
-    document.querySelector('.add-btn').addEventListener('click', function () {
+    var ctrlAddItem = function () {
         //  1. Get Input Data
         //  2. Add item on main controller
         //  3. Add item on UI
         //  4. Calculate Budget
         //  5. Display the Budget on UI
+        console.log("New Item Added;")
+    };
 
+    document.querySelector('.add-btn').addEventListener('click', ctrlAddItem);
+
+
+    document.addEventListener('keypress', function (event) {
+
+
+        if (event.key === "Enter" || event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
     });
 
 })(budgetController, UIController);
